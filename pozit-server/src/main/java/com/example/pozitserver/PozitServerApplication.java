a9ec87e0798cae224f87e42aeb611beab590c63e
1224kang <cherrykang2003@gmail.com>
2026-07-04 04:31:25 +0900

    setting: postgre docker 세팅

---
+++ pozit-server/src/main/java/com/example/pozitserver/PozitServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pozitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PozitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PozitServerApplication.class, args);
+    }
+
+}
---
+++ pozit-server/src/test/java/com/example/pozitserver/PozitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pozitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PozitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
