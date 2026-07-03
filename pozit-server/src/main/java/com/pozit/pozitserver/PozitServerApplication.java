dbc927a4d18b3f6f45a71c39b5299f80e1ff1178
1224kang <cherrykang2003@gmail.com>
2026-07-04 04:35:48 +0900

    setting: 디렉토리 명 변경

--- pozit-server/src/main/java/com/pozit/pozitserver/PozitServerApplication.java
+++ pozit-server/src/main/java/com/pozit/pozitserver/PozitServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.pozitserver;
+package com.pozit.pozitserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pozit-server/src/test/java/com/pozit/pozitserver/PozitServerApplicationTests.java
+++ pozit-server/src/test/java/com/pozit/pozitserver/PozitServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pozitserver;
+package com.pozit.pozitserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
