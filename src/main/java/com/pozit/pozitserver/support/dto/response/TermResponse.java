fec09affa877cfcea547cc7d6a7bf87b4ef46f19
Tulipurple <164765964+Tulipurple@users.noreply.github.com>
2026-08-08 01:49:24 +0900

    feat: 시행일 추가

--- src/main/java/com/pozit/pozitserver/support/dto/response/TermResponse.java
+++ src/main/java/com/pozit/pozitserver/support/dto/response/TermResponse.java
@@ -2,6 +2,8 @@ package com.pozit.pozitserver.support.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
+
 @Schema(description = "약관 정보")
 public record TermResponse(
         @Schema(description = "약관 제목", example = "이용약관")
@@ -11,5 +13,8 @@ public record TermResponse(
         String content,
 
         @Schema(description = "약관 버전", example = "1.0")
-        String version
+        String version,
+
+        @Schema(description = "시행일", example = "2026-01-01")
+        LocalDate effectiveDate
 ) {}
